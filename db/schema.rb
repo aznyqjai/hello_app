55eb77d72a1792a5d2594f268737dedb8e04c5d9
qczhang18 <aznyqjai@gmail.com>
2017-01-01 23:54:21 -0500

    migrated again testing

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170102042052) do
+ActiveRecord::Schema.define(version: 20170102042813) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.string   "commentable_type"
+    t.integer  "commentable_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.index ["commentable_id"], name: "index_comments_on_commentable_id", using: :btree
+    t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id", using: :btree
+  end
 
   create_table "enrollments", force: :cascade do |t|
     t.integer  "game_id"
